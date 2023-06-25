2d416e59534a3ebc3034b8d250e33514e1c44bed
shahin-shohan <56084608+shahin-shohan@users.noreply.github.com>
2023-06-25 23:32:53 +0600

    add all

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,44 +1,36 @@
 import React, { Fragment } from 'react';
-import About from '../../components/about/about';
-import BackToTop from '../../components/backToTop/backToTop';
+import Navbar from '../../components/Navbar/Navbar';
 import ContactArea from '../../components/ContactArea';
 import Hero from '../../components/hero/hero';
 import Marquee from '../../components/marque/marque';
-import Navbar from '../../components/Navbar/Navbar';
-import ProjectSection from '../../components/ProjectSection/ProjectSection';
+import Newslatter from '../../components/Newslatter/Newslatter';
+import Vision from '../../components/Vision/Vision';
+import CalenderSection from '../../components/CalenderSection/CalenderSection';
 import ServiceSection from '../../components/ServiceSection/ServiceSection';
-import Testimonial from '../../components/Testimonial/Testimonial';
-import { Element } from 'react-scroll'
-import BlogSection from '../../components/BlogSection/BlogSection';
+import CtaSection from '../../components/CtaSection/CtaSection';
+import FaqSection from '../../components/FaqSection/FaqSection';
+import PartnerSlider from '../../components/partner';
+import Merch from '../../components/Merch/Merch';
+import BackToTop from '../../components/backToTop/backToTop';
 import Footer from '../../components/footer/Footer';
-import Pricing from '../../components/Pricing';
 
 const HomePage = () => {
     return (
         <Fragment>
             <Navbar />
-            <Element name='home'>
-                <Hero />
-            </Element>
-            <Element name='about'>
-                <About />
-            </Element>
-            <Element name='service'>
-                <ServiceSection />
-            </Element>
-            <Marquee />
-            <Element name='project'>
-                <ProjectSection />
-            </Element>
-            <Testimonial />
-            <Element name='blog'>
-                <BlogSection />
-            </Element>
-            <Pricing />
-            <Element name='contact'>
-                <ContactArea />
-            </Element>
-            <Footer />
+            <Hero />
+            <Marquee/>
+            <Newslatter/>
+            <Vision/>
+            <CalenderSection/>
+            <ServiceSection/>
+            <CtaSection/>
+            <FaqSection/>
+            <PartnerSlider/>
+            <Merch/>
+            <ContactArea/>
+            <Marquee/>
+            <Footer/> 
             <BackToTop />
         </Fragment>
     )
