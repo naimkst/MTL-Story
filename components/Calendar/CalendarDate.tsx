439803b9caf4dac8372a0f643e72017342181ab7
naimkst <naimkst@gmail.com>
2023-07-27 00:05:38 +0600

    update

--- components/Calendar/CalendarDate.tsx
+++ components/Calendar/CalendarDate.tsx
@@ -42,13 +42,7 @@ export const CalendarDate = ({ setCalendarDate, eventId, eventLists }: any) => {
           <div className="row">
             <div className="event-wrap">
               <h2></h2>
-              @//@ts-ignore
-              <Slide
-                cascade
-                direction="up"
-                triggerOnce={false}
-                duration={Number(500)}
-              >
+              <Slide cascade direction="up" triggerOnce="false" duration="500">
                 {eventLists?.map((item: any, index: number) => (
                   <div
                     onClick={() => {
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["mtlstory-admin.outrivalmarkets.com"],
   },
+  experimental: {
+    appDir: false,
+  },
 };
 
 module.exports = nextConfig;
